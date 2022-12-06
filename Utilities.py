9cfff10d560088149c29c8670e197f1e7ed426a6
Bharathwaj <bharathwajist@gmail.com>
2022-12-06 10:02:34 +0530

    Import utility file in Spark project for exection

---
+++ Utilities.py
@@ -0,0 +1,11 @@
+import os
+
+
+# Setting JAVA_HOME system environment path for execution
+os.environ['JAVA_HOME'] = '/home/bharathwaj/Applications/jdk1.8.0_231'
+
+# Setting SPARK_HOME system environment path for execution
+os.environ['SPARK_HOME'] = '/home/bharathwaj/Applications/spark-3.3.1-bin-hadoop2'
+
+# import this file into your project whilst developing an application
+# using PyCharm for smooth execution
